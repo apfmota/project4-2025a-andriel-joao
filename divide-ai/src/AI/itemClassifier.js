79465da49dce2c1fa0643d1f753cfec3b1b0935e
Andriel Prieto <apmota@inf.ufsm.br>
2025-11-06 23:40:28 -0300

    Change API key in itemClassifier.js
    Updated API key for the askTheOracle function.

--- divide-ai/src/AI/itemClassifier.js
+++ divide-ai/src/AI/itemClassifier.js
@@ -1,6 +1,6 @@
 async function askTheOracle(question) {
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=AIzaSyCHJcXAdNNI56kegNsIuMlUc58TTODHMEc",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=579708175877-fgi1k76bfaa2vjkmef7m8dll1bc7766p.apps.googleusercontent.com",
     {
       method: "POST",
       headers: {
